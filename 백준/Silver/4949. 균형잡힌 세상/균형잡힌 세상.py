e5a9e8c0f1bb24193825501e5ab8468db6b071b1
Sang-yeon <119920910+codeeon@users.noreply.github.com>
2024-01-27 00:07:56 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 336 ms, Memory: 112388 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,32 @@
+# import sys
+
+pair = {
+    ')' : '(',
+    ']' : '[',
+}
+opener = '(['
+closer = ')]'
+
+while True:
+    case = input().rstrip()
+    if ' ' not in case and case == '.':
+        break
+
+    stack = []
+    result = True
+
+    for char in case:
+        if char in opener:
+            stack.append(char)
+        elif char in closer:
+            if not stack:
+                result = False
+                break
+            top = stack.pop()
+            if pair[char] != top:
+                result = False
+            
+    if result == False or len(stack) > 0:
+        print('no')
+    else:
+        print('yes')
