5c943a113e91d6dd307d67747ccc25bf00d5f3ae
Sang-yeon <119920910+codeeon@users.noreply.github.com>
2024-02-02 04:58:31 +0900

    [Silver III] Title: 크리스마스 선물, Time: 128 ms, Memory: 40920 KB -BaekjoonHub

--- 백준/Silver/14235. 크리스마스 선물/크리스마스 선물.py
+++ 백준/Silver/14235. 크리스마스 선물/크리스마스 선물.py
@@ -1,19 +1,17 @@
-import sys, heapq#, collections
+import sys, heapq
 
 n = int(sys.stdin.readline().rstrip())
 
 present = []
 
 for i in range(n):
-    # a = collections.deque(sys.stdin.readline().rstrip().split())
-    a = sys.stdin.readline().rstrip().split()
+    a = list(map(int, sys.stdin.readline().rstrip().split()))
 
-    if a[0] == '0':
+    if a[0] == 0:
         if len(present) == 0:
             print(-1)
         else:
             print(heapq.heappop(present) * -1)
     else:
-        # a.popleft()
         for i in range(1, len(a)):
-            heapq.heappush(present, int(a[i]) * -1)
+            heapq.heappush(present, a[i] * -1)
