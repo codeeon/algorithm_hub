3e4eca7410f9c5a0788ad0568f5de2ef5369f4f7
Sang-yeon <119920910+codeeon@users.noreply.github.com>
2024-02-27 18:00:20 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 124 ms, Memory: 9588 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -4,27 +4,17 @@ const input = fs.readFileSync(filePath).toString().trim().split('\n');
 
 const [a, b] = input.shift().split(' ').map(Number);
 
+//  유클리드 호제법
+const findGcd = (a, b) => {
+  const remainder = a % b;
+  if (remainder === 0) return b;
+  return findGcd(b, remainder);
+};
+
+let lcm = (a * b) / findGcd(a, b);
+
 const solution = (a, b) => {
-  let x = Math.max(a, b);
-  let y = Math.min(a, b);
-
-  let gcd = 1;
-  let lcm = x * y;
-
-  for (let i = y; i > 0; i--) {
-    if (x % i === 0 && y % i === 0) {
-      gcd = i;
-      break;
-    }
-  }
-  for (let i = x; i <= lcm; i += x) {
-    if (i % x === 0 && i % y === 0) {
-      lcm = i;
-      break;
-    }
-  }
-
-  return `${gcd}\n${lcm}`;
+  return `${findGcd(a, b)}\n${lcm}`;
 };
 
 console.log(solution(a, b));
