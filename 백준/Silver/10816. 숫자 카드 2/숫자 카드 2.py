a6a63adc3dbf096666f2c248f0db916c6febe3d4
Sang-yeon <119920910+codeeon@users.noreply.github.com>
2025-08-05 21:48:10 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 712 ms, Memory: 238800 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,4 +1,5 @@
 import sys
+import bisect
 
 input = sys.stdin.read
 data = list(map(int, input().split()))
@@ -8,30 +9,12 @@ A = sorted(data[1:N+1])
 M = data[N+1]
 X = data[N+2:]
 
-def lowEnd(arr, target):
-    left, right = 0, len(arr)
-    while left < right:
-        mid = (left + right) // 2
-        if arr[mid] < target:
-            left = mid + 1
-        else:
-            right = mid
-    return left
-
-def highEnd(arr, target):
-    left, right = 0, len(arr)
-    while left < right:
-        mid = (left + right) // 2
-        if arr[mid] <= target:
-            left = mid + 1
-        else:
-            right = mid
-    return left
-
 result = []
 
 for x in X:
-    count = highEnd(A, x) - lowEnd(A, x)
+    left = bisect.bisect_left(A, x)
+    right = bisect.bisect_right(A, x)
+    count = right - left
     result.append(str(count))
 
 print(' '.join(result))
