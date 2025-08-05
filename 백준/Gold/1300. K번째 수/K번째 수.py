a5dd18995e65ab409998d2f34beabd65050d0d32
Sang-yeon <119920910+codeeon@users.noreply.github.com>
2025-08-05 20:34:42 +0900

    [Gold I] Title: K번째 수, Time: 172 ms, Memory: 109544 KB -BaekjoonHub

---
+++ 백준/Gold/1300. K번째 수/K번째 수.py
@@ -0,0 +1,25 @@
+import sys
+
+input = sys.stdin.readline
+N = int(input())
+k = int(input())
+
+def countLessOrEqual(x):
+    cnt = 0
+    for i in range(1, N+1):
+        cnt += min(N, x // i)
+    return cnt
+
+left, right = 1, N * N
+
+answer = 0
+
+while left <= right:
+    mid = (left + right) // 2
+    if countLessOrEqual(mid) >= k:
+        answer = mid
+        right = mid - 1
+    else:
+        left = mid + 1
+
+print(answer)
